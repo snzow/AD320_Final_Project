9cf54c32e626425b2a08ef0f271b7d64ab529dac
Yen Ton <yenton@yens-air.lan>
2023-12-09 18:59:01 -0800

    all features with fake data

--- Public/Venue.js
+++ Public/Venue.js
@@ -1,1 +1,133 @@
-Venue.js
+(function () {
+
+    const API_ROOT = "http://localhost:3001";
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // Fetch bands and their available time slots
+        fetchAvailableBands();
+        // Fetch venue reservations when the page loads
+        fetchVenueReservations();
+    });
+
+    function fetchUserVenueId() {
+        const username = localStorage.getItem('username');
+        if (username) {
+            return fetch(`${API_ROOT}/api/userInfo/${username}`)
+                .then(response => response.json())
+                .then(user => {
+                    const venueId = user.managerOf;
+                    return venueId; 
+                })
+                .catch(error => {
+                    console.error('Error fetching venueId:', error);
+                });
+        } else {
+            return Promise.reject('No username found in localStorage');
+        }
+    }
+    
+    async function fetchVenueReservations() {
+        try {
+            const venueId = await fetchUserVenueId();
+            if (!venueId) {
+                console.error('No venue ID found for the logged-in user.');
+                return; // Exit the function if no venue ID is found
+            }
+    
+            const response = await fetch(`${API_ROOT}/api/venue_reservations/${venueId}`);
+            const reservations = await response.json();
+    
+            const reservationsList = document.getElementById('reservations-display');
+            reservationsList.innerHTML = ''; // Clear any existing reservation entries
+    
+            reservations.forEach(reservation => {
+                const reservationEntry = document.createElement('div');
+                reservationEntry.classList.add('reservation-entry');
+                reservationEntry.innerHTML = `
+                    <p>Date: ${reservation.date}</p>
+                    <p>Start Time: ${reservation.startTime}</p>
+                    <p>End Time: ${reservation.endTime}</p>
+                    <p>Band: ${reservation.bandName}</p>
+                `;
+                reservationsList.appendChild(reservationEntry);
+            });
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    function handleReservation(event) {
+        event.preventDefault();
+        
+        const date = document.getElementById('date').value;
+        const startTime = document.getElementById('start-time').value;
+        const endTime = document.getElementById('end-time').value;
+        const bandName = document.getElementById('bandName').value;
+        
+        // Constructing the reservation data object
+        const reservationData = {
+            date: date,
+            startTime: startTime,
+            endTime: endTime,
+            bandName: bandName
+        };
+
+        // API call to reserve a band
+        fetch('/api/reserve', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(reservationData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // Handle successful reservation
+                alert(`Reservation confirmed! Confirmation ID: ${data.reservationId}`);
+                // Optionally, clear the form or redirect the user
+            } else {
+                // Handle reservation failure
+                alert('Reservation failed. Please try again.');
+            }
+        })
+        .catch(error => {
+            console.error('Error making reservation:', error);
+            alert('An error occurred while making the reservation.');
+        });
+    }
+
+    function fetchAvailableBands() {
+        fetch('http://localhost:3001/api/allBandAvailabilities')
+            .then(response => response.json())
+            .then(bands => {
+                displayAvailableBands(bands);
+            })
+            .catch(error => {
+                console.error('Error fetching available bands:', error);
+            });
+    }
+    
+    function displayAvailableBands(bands) {
+        let bandsList = '<ul>';
+        for (let band of bands) {
+            bandsList += `<li>${band.name}<br>Available Times: <ul>`;
+            for (let time of band.availableTimes) {
+                bandsList += `
+                    <li>
+                        ${time.timeString} 
+                        <button onclick="reserveBandTime('${time.timeId}')">Reserve</button>
+                    </li>`;
+            }
+            bandsList += `</ul></li>`;
+        }
+        bandsList += '</ul>';
+        document.getElementById('bands-display').innerHTML = bandsList;
+    }
+    
+    function reserveBandTime(timeId) {
+        console.log(`Reserving time ID ${timeId}`);
+        // Implement the reservation logic here
+    }
+
+})();
--- Public/band.js
+++ Public/band.js
@@ -1,23 +1,96 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Load booking schedule data and generate the schedule
-    loadBookingSchedule();
-});
+(async function () {
+    const API_ROOT = "http://localhost:3001";
+    let bookingSchedule = [];
 
-function loadBookingSchedule() {
-    // Simulated booking schedule data (replace with your data source)
-    const bookingSchedule = [
-        { date: '2023-12-10', venue: 'Venue A' },
-        { date: '2023-12-15', venue: 'Venue B' },
-        // Add more booking entries as needed
-    ];
-
-    const bookingScheduleContainer = document.getElementById('booking-schedule');
-
-    // Loop through booking data and create schedule entries
-    bookingSchedule.forEach(booking => {
-        const scheduleEntry = document.createElement('div');
-        scheduleEntry.classList.add('booking-entry');
-        scheduleEntry.innerHTML = `<p>Date: ${booking.date}</p><p>Band: ${booking.band}</p>`;
-        bookingScheduleContainer.appendChild(scheduleEntry);
+    document.addEventListener('DOMContentLoaded', async () => {
+        try {
+            const bandId = await fetchUserBandId(); 
+            if (!bandId) {
+                throw new Error('Band ID not found');
+            }
+            await fetchBandSchedule(bandId);
+            
+            // Add event listener to the band booking form
+            const availableTimeForm = document.getElementById('availableTime-form');
+            if (availableTimeForm) {
+                availableTimeForm.addEventListener('submit', availableTimeUpdate);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
     });
-}
+
+    function fetchUserBandId() {
+        const username = localStorage.getItem('username');
+        if (username) {
+            return fetch(`${API_ROOT}/api/userInfo/${username}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then(user => {
+                    const bandId = user.managerOf;
+                    return bandId; 
+                })
+                .catch(error => {
+                    console.error('Error fetching bandId:', error);
+                    throw error; // Rethrow the error to be caught in the outer try-catch
+                });
+        } else {
+            return Promise.reject('No username found in localStorage');
+        }
+    }
+    
+    async function fetchBandSchedule(bandId) {
+        try {
+            const response = await fetch(`${API_ROOT}/api/band_schedule/${bandId}`);
+            const scheduleData = await response.json();
+            loadBookingSchedule(scheduleData);
+        } catch (error) {
+            console.error('Error fetching band schedule:', error);
+        }
+    }
+
+    function loadBookingSchedule(scheduleData) {
+        const bookingScheduleContainer = document.getElementById('schedule-display');
+        
+        // Clear existing schedule entries
+        bookingScheduleContainer.innerHTML = '';
+
+        // Loop through booking data and create schedule entries
+        scheduleData.forEach(booking => {
+            const scheduleEntry = document.createElement('div');
+            scheduleEntry.classList.add('booking-entry');
+            scheduleEntry.innerHTML = `
+                <p>Date: ${booking.date}</p>
+                <p>Start Time: ${booking.startTime}</p>
+                <p>End Time: ${booking.endTime}</p>
+                <p>Venue: ${booking.venueName}</p>
+            `;
+            
+            bookingScheduleContainer.appendChild(scheduleEntry);
+        });
+    }
+
+    function availableTimeUpdate(event) {
+        event.preventDefault();
+        const date = document.getElementById('date').value;
+        const startTime = document.getElementById('start-time').value;
+        const venue = document.getElementById('venue').value;
+
+        // Add the new booking to the bookingSchedule array
+        bookingSchedule.push(newBooking);
+
+        // Reload the updated schedule
+        loadBookingSchedule(bookingSchedule);
+        
+        // Clear the form fields
+        document.getElementById('date').value = '';
+        document.getElementById('start-time').value = '';
+        document.getElementById('end-time').value = '';
+        document.getElementById('venue').value = '';
+    }
+
+})();
